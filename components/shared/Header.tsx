6438f7c7bfab4b555bc1c2d2ad8fa727918c9336
stephane.malho <stepahen.malho@gmail.com>
2023-12-17 17:40:43 +0100

    ✨ add Image link

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-46 "> 
+          <Image alt="Evently logo" src="/assets/images/logo.svg" width={128} height={36} />
+        </Link>
+        <div className="flex w-32 justify-end gap-3">
+          
+        </div>
+      </div>
+    </header>
   )
 }
 
